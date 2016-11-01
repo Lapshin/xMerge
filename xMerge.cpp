841effe6382003a65dd14f4008cecb725f20b45d
Lapshin <alexey.lapshin@eltex.loc>
2016-11-01 12:14:47 +0700

    Hook only on gpon and ma4000 projects

--- xMerge.cpp
+++ xMerge.cpp
@@ -15,6 +15,15 @@ void replaceWhitespaces(string &s)
 	s = regex_replace(s, regex("\\s*,*"), "");
 }
 
+void ReplaceStringInPlace(std::string& subject, const std::string& search,
+                          const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+}
+
 vector<unsigned> parseXMergeRevisions(string s)
 {
 	vector<unsigned> revisionsVector;
@@ -62,11 +71,7 @@ vector<unsigned> parseXMergeRevisions(string s)
 				tmp = x.substr(start, end - start);
 				tmp = regex_replace(tmp, regex("-r"), " ");
 			}
-			if (regex_match(tmp, regex("^[\\d\\s]+$")) == false)
-			{
-				cerr << "revision number expected, but value is " << tmp << endl;
-				exit(1);
-			}
+
 			stringstream(tmp) >> range_start >> range_end;
 			revisionsVector.push_back(range_start);
 			if (range_end != 0)
@@ -108,6 +113,7 @@ class SvnInfo
 {
 private:
 	string repos;
+	string propsPath;
 	string txn;
 	unsigned shard;
 	string message;
@@ -116,6 +122,10 @@ public:
 	vector<SvnRevisionInfo> revisionsList;
 	SvnInfo(string, string);
 	void extractValueOfKey(string path, string key, string &value);
+	void buildMessage();
+	void editTransactionInfo();
+	void getSharded();
+	bool isItMyProject();
 };
 
 void SvnInfo::extractValueOfKey(string path, string key, string &value) {
@@ -148,138 +158,157 @@ void SvnInfo::extractValueOfKey(string path, string key, string &value) {
 	}
 }
 
+void SvnInfo::buildMessage() {
+	vector<unsigned> v = parseXMergeRevisions(this->message);
+	if (v.size() == 0) {
+		exit(0);
+	}
+	stringstream mergeStream;
+	mergeStream << "[MERGE";
+	for (int i = 0; i < v.size(); i++) {
+		if (i > 0) {
+			mergeStream << ",";
+		}
+		mergeStream << " r" << v.at(i);
+	}
+	mergeStream << this->message.substr(this->message.find_first_of(']'));
+	for (int i = 0; i < v.size(); i++) {
+		SvnRevisionInfo rInfo = SvnRevisionInfo(v.at(i));
+		string keyAuthor = "svn:author";
+		string keyLog = "svn:log";
+		stringstream revpropsPath;
+		revpropsPath << this->repos << "/db/revprops/" << rInfo.revision / this->shard << "/" << rInfo.revision;
+		string revpropsPath_s = revpropsPath.str();
+		extractValueOfKey(revpropsPath_s, keyAuthor, rInfo.author);
+		extractValueOfKey(revpropsPath_s, keyLog, rInfo.message);
+		stringstream revsPath;
+		revsPath << this->repos << "/db/revs/" << rInfo.revision / this->shard << "/" << rInfo.revision;
+		string revsPath_s = revsPath.str();
+		ifstream fin(revsPath_s);
+		if (fin.is_open() == false) {
+			cerr << "Can't open " << revsPath_s << endl;
+			exit(1);
+		}
+		string tmp;
+		while (getline(fin, tmp)) {
+			if (tmp.compare(0, strlen("copyroot:"), "copyroot:") == 0) {
+				string empty_s;
+				int empty_i;
+				stringstream(tmp) >> empty_s >> empty_i >> rInfo.branch
+						>> empty_s;
+				break; /*read only first*/
+			}
+		}
+		fin.close();
+
+		rInfo.message.insert(0, "\t");
+		ReplaceStringInPlace(rInfo.message, string("\n"), string( "\n\t"));
+
+		mergeStream << endl << "r" << rInfo.revision << "::" << rInfo.branch
+				<< "::" << rInfo.author << endl << rInfo.message;
+	}
+	this->buildedMessage = mergeStream.str();
+}
+
+void SvnInfo::editTransactionInfo() {
+	string tmpFile = this->propsPath;
+	string key = "svn:log";
+	string keyStr = "K ";
+	stringstream keyStream;
+	keyStream << keyStr << key.length();
+	keyStr = keyStream.str();
+	tmpFile.append(".tmp");
+	ifstream fin(this->propsPath);
+	ofstream of(tmpFile);
+	string tmp;
+	while (getline(fin, tmp)) {
+		if (tmp.compare(keyStr) == 0 && getline(fin, tmp)
+				&& tmp.compare(key) == 0 && getline(fin, tmp)) {
+			if (tmp.compare(0, strlen("V "), "V ") != 0) {
+				cerr << "Unexpected string after \"K\" " << tmp << endl;
+				exit(1);
+			}
+			of << keyStr << endl;
+			of << key << endl;
+			of << "V " << this->buildedMessage.length() << endl;
+			of.write(this->buildedMessage.c_str(),
+					this->buildedMessage.length());
+			of << endl;
+			getline(fin, tmp);
+			continue;
+		}
+		of << tmp << endl;
+	}
+	fin.close();
+	of.close();
+	remove(this->propsPath.c_str());
+	rename(tmpFile.c_str(), this->propsPath.c_str());
+}
+
+void SvnInfo::getSharded() {
+	string tmp;
+	string formatPath = this->repos;
+	formatPath.append("/db/format");
+	ifstream fin(formatPath);
+	if (fin.is_open() == false) {
+		cerr << "Can't open " << formatPath << endl;
+		exit(1);
+	}
+	this->shard = 0;
+	string s_token = string("layoutsharded");
+	while (getline(fin, tmp)) {
+		replaceWhitespaces(tmp);
+		transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+		if (tmp.compare(0, s_token.length(), s_token) == 0) {
+			tmp.erase(0, s_token.length());
+			stringstream(tmp) >> this->shard;
+			break;
+		}
+	}
+	fin.close();
+}
+
+bool SvnInfo::isItMyProject() {
+	string changesPath = this->repos;
+	changesPath.append("/db/transactions/" + this->txn + ".txn/changes");
+	bool isMyProject = false;
+	string tmp;
+	ifstream fin(changesPath);
+	if (fin.is_open() == false) {
+		cerr << "Can't open " << changesPath << " (bad transaction)" << endl;
+		exit(1);
+	}
+	regex myProjects(".*gpon.*|.*ma4000.*");
+	while (getline(fin, tmp)) {
+		isMyProject = regex_match(tmp, myProjects);
+		if (isMyProject == true) {
+			break;
+		}
+	}
+	fin.close();
+	return isMyProject == false;
+}
+
 SvnInfo::SvnInfo(string repos, string txn)
 {
 	this->repos = repos;
 	this->txn = txn;
 
-	string propsPath = repos;
-	propsPath.append("/db/transactions/" + txn + ".txn/props");
-	{
-		string tmp;
-		string formatPath = repos;
-		formatPath.append("/db/format");
-		ifstream fin(formatPath);
-		if(fin.is_open() == false)
-		{
-			cerr << "Can't open " << formatPath << endl;
-			exit(1);
-		}
-		this->shard = 0;
+	this->propsPath = repos;
+	this->propsPath.append("/db/transactions/" + txn + ".txn/props");
 
-		string s_token = string ("layoutsharded");
-		while(getline(fin, tmp))
-		{
-			replaceWhitespaces(tmp);
-			transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
-			if(tmp.compare(0, s_token.length(), s_token) == 0)
-			{
-				tmp.erase(0, s_token.length());
-				stringstream(tmp) >> this->shard;
-				break;
-			}
-		}
-		fin.close();
-	}
-	{
-		string key = "svn:log";
-		extractValueOfKey(propsPath, key, this->message);
-	}
-	{
-		vector<unsigned> v = parseXMergeRevisions(this->message);
-		if(v.size() == 0)
-		{
-			exit(0);
-		}
-		stringstream mergeStream;
-		mergeStream << "[MERGE";
-		for(int i = 0; i < v.size(); i++)
-		{
-			if(i > 0)
-			{
-				mergeStream << ",";
-			}
-			mergeStream << " r" << v.at(i);
-		}
-		mergeStream << this->message.substr(this->message.find_first_of(']'));
-
-
-		for(int i = 0; i < v.size(); i++)
-		{
-			SvnRevisionInfo rInfo = SvnRevisionInfo(v.at(i));
-			string keyAuthor = "svn:author";
-			string keyLog = "svn:log";
-			stringstream revpropsPath;
-			revpropsPath << repos << "/db/revprops/" << rInfo.revision/this->shard << "/" << rInfo.revision;
-			string revpropsPath_s = revpropsPath.str();
-			extractValueOfKey(revpropsPath_s, keyAuthor, rInfo.author);
-			extractValueOfKey(revpropsPath_s, keyLog, rInfo.message);
-
-			stringstream revsPath;
-			revsPath << repos << "/db/revs/" << rInfo.revision/this->shard << "/" << rInfo.revision;
-			string revsPath_s = revsPath.str();
-			ifstream fin(revsPath_s);
-			if (fin.is_open() == false) {
-				cerr << "Can't open " << revsPath_s << endl;
-				exit(1);
-			}
-			string tmp;
-
-			while(getline(fin, tmp))
-			{
-				if(tmp.compare(0, strlen("copyroot:"), "copyroot:") == 0)
-				{
-					string empty_s;
-					int empty_i;
-					stringstream(tmp) >> empty_s >> empty_i >> rInfo.branch >> empty_s;
-					break; /*read only first*/
-				}
-			}
-			fin.close();
-			mergeStream << endl << "r" << rInfo.revision << "::" << rInfo.branch << "::" << rInfo.author << "::message:" << rInfo.message;
-		}
-		this->buildedMessage = mergeStream.str();
+	if(isItMyProject() == false) {
+		exit(0);
 	}
 
+	getSharded();
 
-	{
-		int valueLength;
-		string tmpFile = propsPath;
-		string key = "svn:log";
-		string keyStr = "K ";
-		stringstream keyStream;
-		keyStream << keyStr << key.length();
-		keyStr = keyStream.str();
-		tmpFile.append(".tmp");
-		ifstream fin(propsPath);
-		ofstream of(tmpFile);
-		string tmp;
-		while(getline(fin, tmp))
-		{
-			if (tmp.compare(keyStr) == 0 && getline(fin, tmp)
-					&& tmp.compare(key) == 0 && getline(fin, tmp))
-			{
-				if (tmp.compare(0, strlen("V "), "V ") != 0)
-				{
-					cerr << "Unexpected string after \"K\" " << tmp << endl;
-					exit(1);
-				}
-				stringstream(tmp) >> valueLength;
-				of << keyStr << endl;
-				of << key << endl;
-				of << "V " << this->buildedMessage.length() << endl;
-				of.write(this->buildedMessage.c_str(), this->buildedMessage.length());
-				of << endl;
-				getline(fin, tmp);
-				continue;
-			}
-			of << tmp << endl;
-		}
-		fin.close();
-		of.close();
-		remove(propsPath.c_str());
-		rename(tmpFile.c_str(), propsPath.c_str());
-	}
+	string key = "svn:log";
+	extractValueOfKey(this->propsPath, key, this->message);
+
+	buildMessage();
+
+	editTransactionInfo();
 }
 
 int main(int argc, char **argv)
